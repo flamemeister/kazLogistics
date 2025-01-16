0572b25178265f4b959320242695842b7932b2e5
Ubuntu <aldiyar.saken123@gmail.com>
2025-01-16 17:47:05 +0000

    prod

--- src/components/Services/Avia.jsx
+++ src/components/Services/Avia.jsx
@@ -61,7 +61,7 @@ const AviaServices = () => {
         <div className="absolute inset-0 bg-black bg-opacity-70"></div>
         <div className="relative flex items-center justify-center text-center h-full">
           <h1 className="text-4xl md:text-5xl font-bold uppercase">
-            Автоперевозки
+            Авиаперевозки
           </h1>
         </div>
       </div>
